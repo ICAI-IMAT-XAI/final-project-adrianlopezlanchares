e73d9891fc5d132fab1b98e4bf75d62e21969305
adrianlopezlanchares <202107370@alu.comillas.edu>
2025-12-26 19:54:20 +0100

    create models to experiment on

---
+++ src/models.py
@@ -0,0 +1,39 @@
+from typing import List
+
+import torch
+from sklearn.ensemble import RandomForestClassifier
+
+
+class MLPModel(torch.nn.Module):
+    def __init__(self, input_dim: int, hidden_dim: List[int], output_dim: int):
+        super(MLPModel, self).__init__()
+        layers = []
+        prev_dim = input_dim
+        for h_dim in hidden_dim:
+            layers.append(torch.nn.Linear(prev_dim, h_dim))
+            layers.append(torch.nn.ReLU())
+            prev_dim = h_dim
+        layers.append(torch.nn.Linear(prev_dim, output_dim))
+        self.network = torch.nn.Sequential(*layers)
+
+
+class LinearRegressionModel(torch.nn.Module):
+    def __init__(self, input_dim: int, output_dim: int):
+        super(LinearRegressionModel, self).__init__()
+        self.linear = torch.nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        return self.linear(x)
+
+
+class RandomForestModel:
+    def __init__(self, n_estimators: int = 100, max_depth: int = None):
+        self.model = RandomForestClassifier(
+            n_estimators=n_estimators, max_depth=max_depth
+        )
+
+    def fit(self, X, y):
+        self.model.fit(X, y)
+
+    def predict(self, X):
+        return self.model.predict(X)
