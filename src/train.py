24fa4edf7d5c2c9aa3ceead1581f3b0bc5c3a4a7
adrianlopezlanchares <202107370@alu.comillas.edu>
2025-12-27 16:54:38 +0100

    add validation

--- src/train.py
+++ src/train.py
@@ -4,16 +4,17 @@ import torch
 def train_torch_model(
     model: torch.nn.Module,
     train_loader: torch.utils.data.DataLoader,
+    val_loader: torch.utils.data.DataLoader,
     criterion,
     optimizer,
     num_epochs: int = 10,
     device: str = "cpu",
 ):
     model.to(device)
-    model.train()
 
     for epoch in range(num_epochs):
         running_loss = 0.0
+        model.train()
         for inputs, labels in train_loader:
             inputs, labels = inputs.to(device), labels.to(device)
 
@@ -27,3 +28,15 @@ def train_torch_model(
 
         epoch_loss = running_loss / len(train_loader.dataset)
         print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {epoch_loss:.4f}")
+
+        model.eval()
+        val_loss = 0.0
+        with torch.no_grad():
+            for inputs, labels in val_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item() * inputs.size(0)
+
+        val_loss /= len(val_loader.dataset)
+        print(f"Validation Loss: {val_loss:.4f}")
