9221d5bf59e9f9e27195abdcce87f9b41affef4c
adrianlopezlanchares <202107370@alu.comillas.edu>
2025-12-29 19:41:05 +0100

    Revert "use simple preprocessor"
    This reverts commit 9be5026683a4836a61c9dafa54e282ca49fac8f6.

--- src/data.py
+++ src/data.py
@@ -18,6 +18,93 @@ def download_data() -> None:
         print("Dataset already exists, skipping download.")
 
 
+def build_preprocessor() -> ColumnTransformer:
+    """Builds a SKLearn preprocessor for the Diabetes dataset
+
+    Returns:
+        ColumnTransformer: Unfitted preprocessor
+    """
+
+    # Define column types
+    ordinal_cols = ["age", "A1Cresult", "max_glu_serum"]
+    binary_cols = ["change", "diabetesMed"]
+    categorical_cols = [
+        "race",
+        "gender",
+        "diag_1",
+        "diag_2",
+        "diag_3",
+        "metformin",
+        "repaglinide",
+        "nateglinide",
+        "chlorpropamide",
+        "glimepiride",
+        "acetohexamide",
+        "glipizide",
+        "glyburide",
+        "tolbutamide",
+        "pioglitazone",
+        "rosiglitazone",
+        "acarbose",
+        "miglitol",
+        "troglitazone",
+        "tolazamide",
+        "examide",
+        "citoglipton",
+        "insulin",
+        "glyburide-metformin",
+        "glipizide-metformin",
+        "glimepiride-pioglitazone",
+        "metformin-rosiglitazone",
+        "metformin-pioglitazone",
+    ]
+
+    # Define orders
+    age_order = [
+        "[0-10)",
+        "[10-20)",
+        "[20-30)",
+        "[30-40)",
+        "[40-50)",
+        "[50-60)",
+        "[60-70)",
+        "[70-80)",
+        "[80-90)",
+        "[90-100)",
+    ]
+    a1c_order = ["None", "Norm", ">7", ">8"]
+    glu_order = ["None", "Norm", ">200", ">300"]
+
+    ordinal_transformer = Pipeline(
+        steps=[
+            ("imputer", SimpleImputer(strategy="constant", fill_value="None")),
+            (
+                "encoder",
+                OrdinalEncoder(categories=[age_order, a1c_order, glu_order]),
+            ),
+        ]
+    )
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            (
+                "ord",
+                ordinal_transformer,
+                ordinal_cols,
+            ),
+            ("bin", OneHotEncoder(drop="if_binary"), binary_cols),
+            (
+                "cat",
+                OneHotEncoder(handle_unknown="ignore", sparse_output=False),
+                categorical_cols,
+            ),
+        ],
+        remainder="passthrough",
+    )
+
+    return preprocessor
+
+
 class DiabetesDataset(Dataset):
     def __init__(self, X, y):
         self.X = torch.tensor(X, dtype=torch.float32)
